bec392a40f32cdfe60e96991cfbac9fc21eebdd4
Tinkergit <nicholas.stagno@gmail.com>
2021-03-12 16:42:42 -0500

    commit tree 8dfe529e4aa6b85b9bc82ad58193bd6192b46294

--- app/application.rb
+++ app/application.rb
@@ -1,6 +1,7 @@
 class Application
 
   @@items = ["Apples","Carrots","Pears"]
+  @@cart=[]
 
   def call(env)
     resp = Rack::Response.new
@@ -13,11 +14,30 @@ class Application
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
+    elsif req.path.match(/cart/)
+      if @@cart.empty?
+        resp.write "Your cart is empty"
+      else
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
+      end
+    elsif req.path.match(/add/)
+      item = req.params["item"]
+      resp.write add_item(item)
     else
-      resp.write "Path Not Found"
+    resp.write "Path Not Found\n"
     end
-
     resp.finish
+  end
+
+  def add_item(item)
+    if @@items.include?(item)
+      @@cart.push(item)
+      return "added #{item}\n"
+    else
+      return "We don't have that item #{item}\n"
+    end
   end
 
   def handle_search(search_term)
